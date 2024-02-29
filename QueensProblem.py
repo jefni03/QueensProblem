107b95f3367cd9c51ffc5f6ad991847821be08aa
jefni03 <nijeffrey2003@gmail.com>
2024-02-28 20:27:38 -0800

    changed diagonal detection

--- QueensProblem.py
+++ QueensProblem.py
@@ -33,11 +33,11 @@ class ChessBoardGUI:
         button.place(x=410, y=100)
 
     def button_click(self):
-        self.solveNQ()
+        self.solve_problem()
         self.update_chessboard()
 
     def update_chessboard(self):
-        self.canvas.delete("all")  # Clear previous chessboard
+        self.canvas.delete("all")  
         for row in range(self.board_size):
             for col in range(self.board_size):
                 x0 = col * self.square_size
@@ -52,41 +52,35 @@ class ChessBoardGUI:
                     self.canvas.create_text(x0 + self.square_size / 2, y0 + self.square_size / 2,
                                             text="♛", font=("Helvetica", 16), fill="red")
 
-    def solveNQ(self):
+    def solve_problem(self):
         global N
         N = self.board_size
 
-        def isSafe(row, col):
-            for i in range(col):
-                if self.chessboard[row][i] == 1:
-                    return False
+        leftDiagonal = [0] * (2 * N - 1)
+        rightDiagonal = [0] * (2 * N - 1)
+        cl = [0] * N
 
-            for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-                if self.chessboard[i][j] == 1:
-                    return False
+        def is_safe(row, col):
+            return leftDiagonal[row - col + N - 1] == rightDiagonal[row + col] == cl[row] == 0
 
-            for i, j in zip(range(row, N, 1), range(col, -1, -1)):
-                if self.chessboard[i][j] == 1:
-                    return False
-
-            return True
-
-        def solveNQUtil(col):
+        def solve_problem_util(col):
             if col >= N:
                 return True
 
             for i in range(N):
-                if isSafe(i, col):
+                if is_safe(i, col):
                     self.chessboard[i][col] = 1
+                    leftDiagonal[i - col + N - 1] = rightDiagonal[i + col] = cl[i] = 1
 
-                    if solveNQUtil(col + 1):
+                    if solve_problem_util(col + 1):
                         return True
 
                     self.chessboard[i][col] = 0
+                    leftDiagonal[i - col + N - 1] = rightDiagonal[i + col] = cl[i] = 0
 
             return False
 
-        solveNQUtil(0)
+        solve_problem_util(0)
 
 if __name__ == "__main__":
     root = tk.Tk()
